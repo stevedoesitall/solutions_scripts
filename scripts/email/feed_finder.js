867562cfb093aaec61f9314a95cc652ba8b5bd8e
Steve Giordano <stephenagiordano@gmail.com>
2020-01-24 16:01:26 -0500

    Tweak to feed_finder

--- scripts/email/feed_finder.js
+++ scripts/email/feed_finder.js
@@ -104,61 +104,67 @@ const save_data = () => {
 
     let total_feeds = 0;
 
-    all_feeds.forEach(feed => {
-        http.get(feed, (cb) => {
-            cb.setEncoding("utf8");
-            let raw_data = "";
-            cb.on("data", (chunk) => { raw_data += chunk; });
-            cb.on("end", () => {
-                try {
-                    total_feeds++;
+    if (all_feeds.length == 0) {
+        console.log("No feeds!");
+    }
+    else {
+        all_feeds.forEach(feed => {
+            
+            http.get(feed, (cb) => {
+                cb.setEncoding("utf8");
+                let raw_data = "";
+                cb.on("data", (chunk) => { raw_data += chunk; });
+                cb.on("end", () => {
+                    try {
+                        total_feeds++;
 
-                    if (raw_data.substr(0,7) == "<h1>404") {
-                        console.log(feed + " does not exist");
-                        feed_obj[feed].name = feed;
-                        feed_obj[feed].exists = "N";
-                        feed_obj[feed].timed_out = "N/A";
-                    }
-
-                    else {
-                        feed_obj[feed].exists = "Y";
-                        const parsed_data = JSON.parse(raw_data);
-
-                        if (typeof parsed_data.feed != "undefined") {
-                            feed_obj[feed].name = parsed_data.feed.name; 
-                            feed_obj[feed].timed_out = "N";
-                        }
-                        else {
+                        if (raw_data.substr(0,7) == "<h1>404") {
+                            console.log(feed + " does not exist");
                             feed_obj[feed].name = feed;
-                            feed_obj[feed].timed_out = "Y";
+                            feed_obj[feed].exists = "N";
+                            feed_obj[feed].timed_out = "N/A";
                         }
-                    }
 
-                    if (total_feeds == all_feeds.length) {
-                        const all_feeds_new = Object.keys(feed_obj);
-                        fs.appendFile(feed_log, `Feed Name@Send Count@Blast Count@Trigger Count@Timed Out@Blast Names@Template Names@Exists` + "\n", (err) => {
-                            if (err) {
-                                console.log("Unable to append to file.");
+                        else {
+                            feed_obj[feed].exists = "Y";
+                            const parsed_data = JSON.parse(raw_data);
+
+                            if (typeof parsed_data.feed != "undefined") {
+                                feed_obj[feed].name = parsed_data.feed.name; 
+                                feed_obj[feed].timed_out = "N";
                             }
-                        });
-                        
-                        all_feeds_new.forEach(feed => {
+                            else {
+                                feed_obj[feed].name = feed;
+                                feed_obj[feed].timed_out = "Y";
+                            }
+                        }
 
-                            console.log(`${feed_obj[feed].name} feed was used ${feed_obj[feed].blast_count + feed_obj[feed].template_count} times.`);
-                            fs.appendFile(feed_log, `${feed_obj[feed].name}@${feed_obj[feed].blast_count + feed_obj[feed].template_count}@${feed_obj[feed].blast_count}@${feed_obj[feed].template_count}@${feed_obj[feed].timed_out}@${feed_obj[feed].blast_names}@${feed_obj[feed].template_names}@${feed_obj[feed].exists}` + "\n", (err) => {
+                        if (total_feeds == all_feeds.length) {
+                            const all_feeds_new = Object.keys(feed_obj);
+                            fs.appendFile(feed_log, `Feed Name@Send Count@Blast Count@Trigger Count@Timed Out@Blast Names@Template Names@Exists` + "\n", (err) => {
                                 if (err) {
                                     console.log("Unable to append to file.");
                                 }
                             });
-                        });
+                            
+                            all_feeds_new.forEach(feed => {
+
+                                console.log(`${feed_obj[feed].name} feed was used ${feed_obj[feed].blast_count + feed_obj[feed].template_count} times.`);
+                                fs.appendFile(feed_log, `${feed_obj[feed].name}@${feed_obj[feed].blast_count + feed_obj[feed].template_count}@${feed_obj[feed].blast_count}@${feed_obj[feed].template_count}@${feed_obj[feed].timed_out}@${feed_obj[feed].blast_names}@${feed_obj[feed].template_names}@${feed_obj[feed].exists}` + "\n", (err) => {
+                                    if (err) {
+                                        console.log("Unable to append to file.");
+                                    }
+                                });
+                            });
+                        }
+                    } 
+                    catch (e) {
+                        console.error(e.message);
                     }
-                } 
-                catch (e) {
-                    console.error(e.message);
-                }
+                });
+                }).on("error", (e) => {
+                    console.error(`Got error: ${e.message}`);
             });
-            }).on("error", (e) => {
-                console.error(`Got error: ${e.message}`);
         });
-    });
+    }
 }
