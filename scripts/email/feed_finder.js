5e5d0b5227d9ed2a189e2957a00d8172ce038ae9
stevedoesitall <stevegiordano@MBA0395.local>
2020-03-09 21:40:06 -0400

    Bit of clean up

--- scripts/email/feed_finder.js
+++ scripts/email/feed_finder.js
@@ -1,3 +1,5 @@
+//Probably a lot of code in here that can be consolidated; task for another day
+
 const path = require("path");
 const creds = path.join(__dirname, "../../ignore/creds.js");
 const feed_log = path.join(__dirname, "../../logs/feeds.txt");
@@ -19,9 +21,6 @@ const end_date = require("../modules/dates.js").end_date;
 
 const feed_obj = {};
 
-let total_blasts = 0;
-let total_templates = 0;
-
 sailthru.apiGet("blast", {
     start_date: start_date,
     end_date: end_date,
@@ -35,8 +34,6 @@ sailthru.apiGet("blast", {
         console.log("Getting blast stats...");
         const blasts = response.blasts;
         blasts.forEach(blast => {
-            total_blasts++;
-
             if (blast.data_feed_url) {
                 if (feed_obj[blast.data_feed_url]) {
                     feed_obj[blast.data_feed_url].blast_count++
@@ -67,8 +64,6 @@ const get_templates = () => {
         else {
             const templates = response.templates;
             templates.forEach(template => {
-                total_templates++;
-
                 if (template.data_feed_url) {
 
                     if (feed_obj[template.data_feed_url]) {
@@ -117,6 +112,9 @@ const save_data = () => {
             }
             else if (feed.substr(0,8) === "https://") {
                 protocol = https;
+            }
+            else {
+                protocol = null;
             }
 
             if (protocol) {
@@ -192,7 +190,6 @@ const save_feed = () => {
     });
     
     all_feeds_new.forEach(feed => {
-
         console.log(`${feed_obj[feed].name} feed was used ${feed_obj[feed].blast_count + feed_obj[feed].template_count} times.`);
         fs.appendFile(feed_log, `${feed_obj[feed].name}@${feed}@${feed_obj[feed].blast_count + feed_obj[feed].template_count}@${feed_obj[feed].blast_count}@${feed_obj[feed].template_count}@${feed_obj[feed].timed_out}@${feed_obj[feed].blast_names}@${feed_obj[feed].template_names}@${feed_obj[feed].exists}` + "\n", (err) => {
             if (err) {
