0124a4e2b241e2b61e40d394e697f640a998792f
Steve Giordano <stephenagiordano@gmail.com>
2020-07-04 19:37:39 -0400

    New audit work

--- scripts/cleanup/su_cleaner.js
+++ scripts/cleanup/su_cleaner.js
@@ -1,1 +1,20 @@
 //Create SU Query Cleanup File
+
+const path = require("path");
+const fs = require("fs");
+const su_folder = path.join(__dirname, "../../su_queries/");
+
+fs.readdir(su_folder, function(err, queries) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        queries.forEach(query => {
+            console.log(su_folder)
+            const folder = path.join(su_folder + "/", query);
+            fs.writeFile(folder, "", function() { 
+                console.log(queries + " file cleared.");
+            }); 
+        });
+    }
+});
